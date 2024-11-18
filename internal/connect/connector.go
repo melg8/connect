c520675dbadae8341af72b36e306cd3bd51cacc9
Melg Eight <public.melg8@gmail.com>
2024-11-19 01:47:55 +0300

    refactor connector

--- internal/connect/connector.go
+++ internal/connect/connector.go
@@ -1,12 +1,8 @@
-// SPDX-FileCopyrightText: © 2024 Melg Eight <public.melg8@gmail.com>
-//
-// SPDX-License-Identifier: MIT
-
 package connect
 
 import (
-	"errors"
 	"fmt"
+	"log"
 	"net"
 	"time"
 )
@@ -18,26 +14,23 @@ type Connector interface {
 
 type TcpConnector struct {
 	serverAddress string
+	timeout       time.Duration
 }
 
-func NewTcpConnector(serverAddress string) *TcpConnector {
+func NewTcpConnector(serverAddress string, timeout time.Duration) *TcpConnector {
 	return &TcpConnector{
 		serverAddress: serverAddress,
+		timeout:       timeout,
 	}
 }
 
 func (c *TcpConnector) Connect() (net.Conn, error) {
-	return net.DialTimeout("tcp", c.serverAddress, time.Second)
+	return net.DialTimeout("tcp", c.serverAddress, c.timeout)
 }
 
 func (c *TcpConnector) Address() string {
 	return c.serverAddress
 }
-
-// Prevents too frequent connections to the login server from the same IP from
-// triggering flood protection.
-// https://gitlab.com/TheDnR/l2j-lisvus/-/blame/main/
-// core/java/net/sf/l2j/util/IPv4Filter.java#L88
 
 type RateLimitedConnector struct {
 	connector    Connector
@@ -45,11 +38,11 @@ type RateLimitedConnector struct {
 	timeout      time.Duration
 }
 
-func NewRateLimitedConnector(connector Connector) *RateLimitedConnector {
+func NewRateLimitedConnector(connector Connector, timeout time.Duration) *RateLimitedConnector {
 	return &RateLimitedConnector{
 		connector:    connector,
-		lastConnTime: time.Now().Add(time.Second * -2),
-		timeout:      time.Second + time.Millisecond*100,
+		lastConnTime: time.Now().Add(-timeout),
+		timeout:      timeout,
 	}
 }
 
@@ -59,20 +52,22 @@ func (c *RateLimitedConnector) Address() string {
 
 func (c *RateLimitedConnector) Connect() (net.Conn, error) {
 	now := time.Now()
-	fmt.Println("Passed " + now.Sub(c.lastConnTime).String() +
-		" from last attempt to connect to server")
+
 	if now.Sub(c.lastConnTime) < c.timeout {
-		fmt.Println("Sleeping for 1 sec")
-		time.Sleep(c.timeout)
+		log.Printf("Sleeping for %v seconds before next attempt", c.timeout.Seconds())
+		time.Sleep(c.timeout - time.Since(now))
+	} else {
+		log.Println("Connection successful, no delay needed")
 	}
-	fmt.Println("Connecting to server: " + c.connector.Address() + " time: " + now.String())
+
 	conn, err := c.connector.Connect()
 	if err != nil {
-		fmt.Printf("Error connecting to server: %v\n", err)
+		log.Printf("Error connecting to server: %v", err)
 		return nil, err
 	}
+
 	c.lastConnTime = now
-	fmt.Println("Connected to server: " + c.connector.Address() + " time: " + now.String())
+	log.Printf("Connected to server: %s at %v", c.connector.Address(), now)
 	return conn, nil
 }
 
@@ -81,7 +76,7 @@ type RetryConnector struct {
 	retries   int
 }
 
-func NewRetriesConnector(connector Connector, retries int) *RetryConnector {
+func NewRetryConnector(connector Connector, retries int) *RetryConnector {
 	return &RetryConnector{connector: connector, retries: retries}
 }
 
@@ -91,25 +86,25 @@ func (c *RetryConnector) Address() string {
 
 func (c *RetryConnector) Connect() (net.Conn, error) {
 	for r := c.retries; r > 0; r-- {
-		fmt.Println("Requesting connection, retries left: " + fmt.Sprint(r))
+		log.Printf("Attempting connection %d/%d", r, c.retries)
 		conn, err := c.connector.Connect()
 		if err != nil {
-			fmt.Printf("Error connecting to server: %v\n", err)
+			log.Printf("Error connecting to server: %v (attempt %d)", err, r)
 			if r > 1 {
-				fmt.Println("Retrying connection...")
+				log.Println("Retrying connection...")
 			}
 			continue
 		} else {
 			return conn, nil
 		}
 	}
-	return nil, errors.New("Failed to connect to server: " + c.Address() +
-		"after " + fmt.Sprint(c.retries) + " retries")
+	return nil, fmt.Errorf("failed to connect to server after %d attempts", c.retries)
 }
 
-func ServerConnector(address string) *RetryConnector {
-	if address == "" {
-		return nil
-	}
-	return NewRetriesConnector(NewRateLimitedConnector(NewTcpConnector(address)), 5)
+func ServerConnector(address string) (*RetryConnector, error) {
+	tcpConnectorTimeout := time.Second * 10
+	betweenAttemptsTimeout := time.Second + time.Millisecond*10
+	tcpConnector := NewTcpConnector(address, tcpConnectorTimeout)
+	connector := NewRateLimitedConnector(tcpConnector, betweenAttemptsTimeout)
+	return NewRetryConnector(connector, 5), nil
 }
