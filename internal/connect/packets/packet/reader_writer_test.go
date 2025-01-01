e163ad508afb6eb7bdf809bf38f7260d3696497a
Melg Eight <public.melg8@gmail.com>
2025-01-01 22:46:03 +0300

    reduce number of statements in single function

--- internal/connect/packets/packet/reader_writer_test.go
+++ internal/connect/packets/packet/reader_writer_test.go
@@ -6,95 +6,113 @@ package packet
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 )
 
-func TestPacketWriterAndReader(t *testing.T) {
-	writer := NewWriter()
+const testStringValue = "some string"
+
+func writeValuesToWriter(writer *Writer) ([]byte, error) {
 	int64Value := int64(1234567890123)
 	err := writer.WriteInt64(int64Value)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	int32Value := int32(123456789)
 	err = writer.WriteInt32(int32Value)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	int16Value := int16(12345)
 	err = writer.WriteInt16(int16Value)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	int8Value := int8(123)
 	err = writer.WriteInt8(int8Value)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	stringValue := "some string"
-	err = writer.WriteStringAsUtf16(stringValue)
+	err = writer.WriteStringAsUtf16(testStringValue)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	keyData := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
 	err = writer.WriteBytes(keyData)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	reader := NewReader(writer.Bytes())
+	return writer.Bytes(), nil
+}
 
-	gotInt64Value, err := reader.ReadInt64()
+func readValuesFromReader(reader *Reader) error {
+	int64Value, err := reader.ReadInt64()
+	if err != nil {
+		return err
+	}
+	if int64Value != 1234567890123 {
+		return fmt.Errorf("Got different int64 value: %d != 1234567890123", int64Value)
+	}
+
+	int32Value, err := reader.ReadInt32()
+	if err != nil {
+		return err
+	}
+	if int32Value != 123456789 {
+		return fmt.Errorf("Got different int32 value: %d != 123456789", int32Value)
+	}
+
+	int16Value, err := reader.ReadInt16()
+	if err != nil {
+		return err
+	}
+	if int16Value != 12345 {
+		return fmt.Errorf("Got different int16 value: %d != 12345", int16Value)
+	}
+
+	int8Value, err := reader.ReadInt8()
+	if err != nil {
+		return err
+	}
+	if int8Value != 123 {
+		return fmt.Errorf("Got different int8 value: %d != 123", int8Value)
+	}
+
+	stringValue, err := reader.ReadStringFromUtf16Format()
+	if err != nil {
+		return err
+	}
+	if stringValue != testStringValue {
+		return fmt.Errorf("Got different string value: %s != %s", stringValue, testStringValue)
+	}
+
+	keyData, err := reader.ReadBytes(16)
+	if err != nil {
+		return err
+	}
+	if !bytes.Equal(keyData, []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}) {
+		return fmt.Errorf("Got different key data: %s != %s", keyData, []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15})
+	}
+
+	return nil
+}
+
+func TestPacketWriterAndReader(t *testing.T) {
+	writer := NewWriter()
+	data, err := writeValuesToWriter(writer)
 	if err != nil {
 		panic(err)
 	}
-	if gotInt64Value != int64Value {
-		t.Errorf("Got different UInt64 value: %d != %d", gotInt64Value, int64Value)
-	}
 
-	gotInt32Value, err := reader.ReadInt32()
-	if err != nil {
-		panic(err)
-	}
-	if gotInt32Value != int32Value {
-		t.Errorf("Got different int32 value: %d != %d", gotInt32Value, int32Value)
-	}
-
-	gotInt16Value, err := reader.ReadInt16()
-	if err != nil {
-		panic(err)
-	}
-	if gotInt16Value != int16Value {
-		t.Errorf("Got different int16 value: %d != %d", gotInt16Value, int16Value)
-	}
-
-	gotInt8Value, err := reader.ReadInt8()
-	if err != nil {
-		panic(err)
-	}
-	if gotInt8Value != int8Value {
-		t.Errorf("Got different int8 value: %d != %d", gotInt8Value, int8Value)
-	}
-
-	gotStringValue, err := reader.ReadStringFromUtf16Format()
-	if err != nil {
-		panic(err)
-	}
-	if gotStringValue != stringValue {
-		t.Errorf("Got different string value: %s != %s", gotStringValue, stringValue)
-	}
-
-	gotKeyData, err := reader.ReadBytes(len(keyData))
-	if err != nil {
-		panic(err)
-	}
-	if !bytes.Equal(gotKeyData, keyData) {
-		t.Errorf("Got different key data: %s != %s", gotKeyData, keyData)
+	reader := NewReader(data)
+	if err := readValuesFromReader(reader); err != nil {
+		t.Error(err)
 	}
 }
 
